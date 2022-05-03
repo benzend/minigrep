6d5abc4fca854b8d2b31b096383eae42665a22f9
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-05-02 22:47:38 -0600

    collect flags and store in vec

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ const FLAG: &str = "--";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let mut flags: Vec<String> = Vec::new();
     
     for arg in args {
         if is_flag(&arg) {
-            println!("{} is a flag", &arg);
-        } else {
-            println!("{} is not a flag", &arg);
+            flags.push(String::from(&arg));
         }
     }
-    
+
 }
 
 fn is_flag(string: &String) -> bool {
