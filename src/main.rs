06c5b8c9719fb28403da71af1ae49bb128c86154
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-05-02 23:01:36 -0600

    feat: now can read files
    Using new flag handler "flag-" to not mess with cargo's flag system
    
    Now split flag args and non flag args

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 use std::env;
+use std::fs;
 
-const FLAG: &str = "--";
+const FLAG: &str = "flag-";
+const READ_FILE_FLAG: &str = "read_file";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let mut non_flags: Vec<String> = Vec::new();
     let mut flags: Vec<String> = Vec::new();
     
     for arg in args {
         if is_flag(&arg) {
-            flags.push(String::from(&arg));
+            flags.push(String::from(&arg[FLAG.len()..]));
+        } else {
+            non_flags.push(String::from(&arg));
         }
+    }
+
+    if flags.contains(&String::from(READ_FILE_FLAG)) {
+        if non_flags.len() == 1 {
+            let file_to_read = &non_flags[0];
+            let contents = fs
+                ::read_to_string(file_to_read)
+                .expect("Something went wrong reading the file");
+
+            println!("{}", contents);
+        }
+
+
     }
 
 }
 
 fn is_flag(string: &String) -> bool {
-    if &string[0..2] == FLAG {
+    if &string[0..FLAG.len()] == FLAG {
         true
     } else {
         false
