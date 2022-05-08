c56ba7be69988400c7f7229af76f40e90d5ce01a
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-05-08 12:39:49 -0600

    clean: moved tests to lib and added new test
    Added insensitive search test

--- src/lib.rs
+++ src/lib.rs
@@ -167,11 +167,22 @@ Pick three.";
     fn case_sensitive() {
       let query = "duct";
       let contents = "\
-  Rust:
-  safe, fast, productive.
-  Pick three.
-  Duct tape.";
-      assert_eq!(vec!["safe, fast, produtive"], search(query, contents));
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+      assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+      let query = "duct";
+      let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+      assert_eq!(vec!["safe, fast, productive.", "Duct tape."], search_case_insensitive(query, contents));
     }
 }
 
